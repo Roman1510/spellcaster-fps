bd6d179ceca7d623495d8bccced68c3191e82fc7
Roman1510 <romanvinnick@gmail.com>
2024-07-06 10:55:20 +0200

    initial setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Box, OrbitControls } from '@react-three/drei'
+import { Color, FogExp2 } from 'three'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Model() {
+  return <Box args={[1, 1, 1]} />
 }
 
-export default App
+export default function App() {
+  return (
+    <div className="canvas-wrapper">
+      <Canvas
+        key="canvas-game"
+        dpr={1}
+        camera={{ fov: 30 }}
+        onCreated={({ scene }) => {
+          scene.background = new Color(0x000000)
+          scene.fog = new FogExp2(0x000000, 0.002)
+        }}
+        gl={{
+          powerPreference: 'high-performance',
+          antialias: false,
+          autoClear: true,
+        }}
+      >
+        <ambientLight />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
+  )
+}
