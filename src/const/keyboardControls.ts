e1c1eac547d86752b0eed3d17bb25518d0823ae4
Roman1510 <romanvinnick@gmail.com>
2025-07-01 09:29:10 +0200

    added azerty

--- src/const/keyboardControls.ts
+++ src/const/keyboardControls.ts
@@ -1,7 +1,7 @@
 export const keyboardControls = [
-  { name: 'forward', keys: ['ArrowUp', 'w', 'W'] },
+  { name: 'forward', keys: ['ArrowUp', 'w', 'W', 'z', 'Z'] },
   { name: 'backward', keys: ['ArrowDown', 's', 'S'] },
-  { name: 'left', keys: ['ArrowLeft', 'a', 'A'] },
+  { name: 'left', keys: ['ArrowLeft', 'a', 'A', 'q', 'Q'] },
   { name: 'right', keys: ['ArrowRight', 'd', 'D'] },
   { name: 'dash', keys: ['Shift'] },
   { name: 'take', keys: ['E', 'e'] },
